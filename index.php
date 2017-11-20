f8fb29c69ce232889c4f2ac0ef208659ad1711c8
Jamie Ullman <ullmanj@stlukesct.org>
2017-11-20 14:01:05 -0500

    Added to comment in index
    minor fix

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
 * Front end people: you should start here
 * Middle (javascript) group: I will help you make another file in order to keep your work private
 * Back end: I don't know what to do with you yet, but it definatly does not include editing this file.
+* Remeber: Whatever you do in any files in the project can and will be seen by any and everyone. So please, lets have some order.
 -->
 </html>
