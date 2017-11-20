003af698008fed1df407b2b53aa2678a0f88528a
Jamie Ullman <ullmanj@stlukesct.org>
2017-11-20 13:50:47 -0500

    Adding 2 files
    .gitignore
    index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<!--This is the index file. Meaning this is where the user will initially be directed.
+* Front end people: you should start here
+* Middle (javascript) group: I will help you make another file in order to keep your work private
+* Back end: I don't know what to do with you yet, but it definatly does not include editing this file.
+-->
+</html>
