e2d88def90e7c6847ca8cc0fe1016e246904cf4e
Jack Briggs <briggsj21@stlukesct.org>
2017-12-01 09:43:36 -0500

    Pull scripts deleted

--- autoPull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-  git clone https://github.com/ullmanj/Vaca4Vets
-  shopt -s extglob
-  rm !(automatePush.php|autoPull.sh)
-  mv Vaca4Vets/*
--- automatePush.php
+++
@@ -1,5 +0,0 @@
-<?php
-  
-  $output = shell_exec('ls -lart');
-  echo "<pre>"$output"</pre>";
-?>
