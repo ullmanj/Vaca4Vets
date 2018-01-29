381157436e28bfd1d8d3e0bf0bd586315e2f62d9
Unknown <briggsj21@stlukesct.org>
2018-01-29 08:13:31 -0500

    Auto stash before merge of "master" and "origin/master"

--- Server_and_SQL_Files/html/sql.php
+++ Server_and_SQL_Files/html/sql.php
@@ -1,6 +1,11 @@
 <html>
 <body>
-hello - Liam
+hi
+<form method="post" enctype="multipart/form-data">
+	<input type="connect" value="Connect" name="connect">
+  	<input type="add" value="ADD" name="ADD VET">
+    <input type="dc" value="disconnect" name="Disconnect">
+</form>
 </body>
 </html>
 <?php
@@ -9,7 +14,18 @@ $servername = "localhost";
 $username = "root";
 $password = "briggs-test";
 $dbname = "V4V";*/
-
+if (isset($_POST['connect'])) 
+{ 
+  openConnection("localhost", "root", "briggs-test", "V4V");
+}  
+if (isset($_POST['add'])) 
+{ 
+  addToVet(1, "Steven", "Rohan", "McGuilligan", "The Marines", "The President", "Oct, 2014 - Active", "1-101-101-1101", "steven@mcguilligan.net", "NEVER", "PABLOWASKILLEDBYANAXEMURDERER");
+}  
+if (isset($_POST['dc'])) 
+{ 
+  closeConnection();
+}  
 function openConnection($servername, $username, $password, $dbname)
 {
     // Create connection
@@ -22,9 +38,9 @@ function openConnection($servername, $username, $password, $dbname)
     return true; 
 }
 //vet functions (vets)
-function addToVet($fName, $mName, $lName, $branch, $rank, $activeDates, $phoneNum, $email, $dolcu, $password){
-    $sql = "INSERT INTO vet (fName, mName, lName, branch, rank, activeDates, phoneNum, email, dolcu, password)
-    VALUES ($fName, $mName, $lName, $branch, $rank, $activeDates, $phoneNum, $email, $dolcu, $password)";
+function addToVet($idN, $fName, $mName, $lName, $branch, $rank, $activeDates, $phoneNum, $email, $dolcu, $password){
+    $sql = "INSERT INTO vets (idN, fName, mName, lName, branch, rank, activeDates, phoneNum, email, dolcu, password)
+    VALUES ($idN, $fName, $mName, $lName, $branch, $rank, $activeDates, $phoneNum, $email, $dolcu, $password)";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
@@ -50,16 +66,22 @@ function getAllVetData()
     //access like a 2d array
     return $data;
 }
-function getVetGroup($id, $group)
+function getVetGroup($idN, $group)
 {
-    $sql = "SELECT " . $group . "FROM vets WHERE id == " . $id;
+    $sql = "SELECT " . $group . "FROM vets WHERE idN == " . $idN;
+    $result = $conn->query($sql);
+    return $result;
+}
+function setVetGroup($idN, $group, $value)
+{
+    $sql = "UPDATE vets SET " . $group . "=\'" . $value . "\' WHERE idN == " . $idN;
     $result = $conn->query($sql);
     return $result;
 }
 //Vendor functions (vendors)
-function addToVendor($fName, $mName, $lName, $type, $phoneNum, $email, $active, $password){
-    $sql = "INSERT INTO vendors (fName, mName, lName, type, phoneNum, email, active, password)
-    VALUES ($fName, $mName, $lName, $type, $phoneNum, $email, $active, $password)";
+function addToVendor($idN, $fName, $mName, $lName, $type, $phoneNum, $email, $active, $password){
+    $sql = "INSERT INTO vendors (idN, fName, mName, lName, type, phoneNum, email, active, password)
+    VALUES ($idN, $fName, $mName, $lName, $type, $phoneNum, $email, $active, $password)";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
@@ -85,14 +107,145 @@ function getAllVendorData()
     //access like a 2d array
     return $data;
 }
-function getVendorGroup($id, $group)
+function getVendorGroup($idN, $group)
 {
-    $sql = "SELECT " . $group . "FROM vendors WHERE id == " . $i;
+    $sql = "SELECT " . $group . "FROM vendors WHERE idN == " . $idN;
     $result = $conn->query($sql);
   
     return $result;
 }
+function setVendorGroup($idN, $group, $value)
+{
+    $sql = "UPDATE vendors SET " . $group . "=\'" . $value . "\' WHERE idN == " . $idN;
+    $result = $conn->query($sql);
+    return $result;
+}
+//donor functions
+function addToDonor($idN, $amount, $purpose){
+    $sql = "INSERT INTO donor (idN, amount, purpose)
+    VALUES ($idN, $amount, $purpose)";
 
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+        return true;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        return false;
+    }
+}
+function getAllDonorData()
+{
+  	$data = array();
+    $sql = "SELECT * FROM donor";
+    $result = $conn->query($sql);
+  	if ($result->num_rows > 0) {
+        // output data of each row
+      	while($row = $result->fetch_array()){//loop to get all results
+            $data[] = $row;//grab everything and store inside array
+     	}
+    } else {
+        echo "0 results";
+    }
+    //access like a 2d array
+    return $data;
+}
+function getDonorGroup($idN, $group)
+{
+    $sql = "SELECT " . $group . "FROM donor WHERE idN == " . $idN;
+    $result = $conn->query($sql);
+  
+    return $result;
+}
+function setDonorGroup($idN, $group, $value)
+{
+    $sql = "UPDATE donor SET " . $group . "=\'" . $value . "\' WHERE idN == " . $idN;
+    $result = $conn->query($sql);
+    return $result;
+}
+//experience functions
+function addToExperience($idN, $sName, $address, $website, $description, $datesA, $fees){
+    $sql = "INSERT INTO experience (idN, sName, address, website, description, datesA, fees)
+    VALUES ($idN, $sName, $address, $website, $description, $datesA, $fees)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+        return true;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        return false;
+    }
+}
+function getAllExperienceData()
+{
+  	$data = array();
+    $sql = "SELECT * FROM experience";
+    $result = $conn->query($sql);
+  	if ($result->num_rows > 0) {
+        // output data of each row
+      	while($row = $result->fetch_array()){//loop to get all results
+            $data[] = $row;//grab everything and store inside array
+     	}
+    } else {
+        echo "0 results";
+    }
+    //access like a 2d array
+    return $data;
+}
+function getExperienceGroup($idN, $group)
+{
+    $sql = "SELECT " . $group . "FROM experience WHERE idN == " . $idN;
+    $result = $conn->query($sql);
+  
+    return $result;
+}
+function setExperienceGroup($idN, $group, $value)
+{
+    $sql = "UPDATE experience SET " . $group . "=\'" . $value . "\' WHERE idN == " . $idN;
+    $result = $conn->query($sql);
+    return $result;
+}
+//vachome functions
+function addToVacHome($idN, $address, $REL, $description, $datesA, $bedrooms, $fees){
+    $sql = "INSERT INTO vacHome (idN, address, REL, description, datesA, bedrooms, fees)
+    VALUES ($idN, $address, $REL, $description, $datesA, $bedrooms, $fees)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+        return true;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        return false;
+    }
+}
+function getAllVacHomeData()
+{
+  	$data = array();
+    $sql = "SELECT * FROM vacHome";
+    $result = $conn->query($sql);
+  	if ($result->num_rows > 0) {
+        // output data of each row
+      	while($row = $result->fetch_array()){//loop to get all results
+            $data[] = $row;//grab everything and store inside array
+     	}
+    } else {
+        echo "0 results";
+    }
+    //access like a 2d array
+    return $data;
+}
+function getVacHomeGroup($idN, $group)
+{
+    $sql = "SELECT " . $group . "FROM vacHome WHERE idN == " . $idN;
+    $result = $conn->query($sql);
+  
+    return $result;
+}
+function setVacHomeGroup($idN, $group, $value)
+{
+    $sql = "UPDATE vacHome SET " . $group . "=\'" . $value . "\' WHERE idN == " . $idN;
+    $result = $conn->query($sql);
+    return $result;
+}
 function closeConnection()
 {
     $conn->close();
