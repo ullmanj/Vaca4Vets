b448da03497cf44df87270870682bf4889242f4a
Unknown <briggsj21@stlukesct.org>
2018-02-07 08:22:29 -0500

    Js/Php Fix

--- Server_and_SQL_Files/html/sql.php
+++ Server_and_SQL_Files/html/sql.php
@@ -1,3 +1,17 @@
+<html>
+<body>
+hi
+<form method="post" enctype="multipart/form-data">
+        <input type="submit" value="Add" name="add">
+          <input type="submit" value="Print" name="printc">
+</form>
+                  <span id="myspan"> OUTPUT </span>
+</body>
+</html>
+
+
+
+
 <?php
 echo "WORK";
 /*
@@ -5,267 +19,72 @@ $servername = "localhost";
 $username = "root";
 $password = "briggs-test";
 $dbname = "V4V";*/
-if (isset($_POST['connect'])) 
+
+if (isset($_POST['add'])) 
 { 
-  echo '<script type="text/javascript">',
-     'document.getElementById("myspan").textContent="WEll";',
-     '</script>';
-  if(openConnection("localhost", "root", "briggs-test", "V4V"))
+  if(addToTable('vets', 46, '\'Steven\'', '\'Rohan\'', '\'Mcguilligan\'', '\'Marines\'', '\'Gunnery Sergeant\'', '\'November 20, 2014 - PRESENT\'', '\'1-011-101-1000\'', '\'steven@mcguilligan.net\'', '\'NEVER\'', '\'StevenMcguilligan1\'', '\'srohan524\'')
   {
-  	echo '<script type="text/javascript">',
-     'document.getElementById("myspan").textContent="Connected";',
+    echo '<script type="text/javascript">',
+     'document.getElementById("myspan").innerHTML ="Added";',
      '</script>';
   }
   else
   {
-    echo '<script type="text/javascript">',
-     'document.getElementById("myspan").textContent="Not Connected";',
+   echo '<script type="text/javascript">',
+     'document.getElementById("myspan").innerHTML ="ERRROORRRRR";',
      '</script>';
   }
-}  
-if (isset($_POST['add'])) 
-{ 
-  echo "HELLLO0";
-  addToVet(1, 1, "Steven", "Rohan", "McGuilligan", "The Marines", "The President", "Oct, 2014 - Active", "1-101-101-1101", "steven@mcguilligan.net", "NEVER", "PABLOWASKILLEDBYANAXEMURDERER");
-}  
-if (isset($_POST['dc'])) 
-{ 
-  closeConnection();
-}  
-function openConnection($servername, $username, $password, $dbname)
-{
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        return false;
+}
+if(isset($_POST['printc']))
+    {
+      $array = array("idN", "first", "middle", "last");
+      selectCol("idN, first, middle, last", "vets", "", $array);
     }
-    return true; 
-}
-//vet functions (vets)
-function addToVet($id, $idN, $fName, $mName, $lName, $branch, $rank, $activeDates, $phoneNum, $email, $dolcu, $password){
-  echo "HELLLO1";
-    $sql = "INSERT INTO vets ($id, idN, fName, mName, lName, branch, rank, activeDates, phoneNum, email, dolcu, password)
-      VALUES ($id, $idN, $fName, $mName, $lName, $branch, $rank, $activeDates, $phoneNum, $email, $dolcu, $password)";
-	echo "HELLLO2";
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        return true;
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-        return false;
+function addToTable($table, $idN, $fName, $mName, $lName, $branch, $rank, $activeDates, $phoneNum, $email, $dolcu, $username, $password){
+    $mysqli = new mysqli("localhost", "root", "briggs-test", "V4V");
+ 
+// Check connection
+    if($mysqli === false){
+         die("ERROR: Could not connect. " . $mysqli->connect_error);
     }
-}
-function getAllVetData()
-{
-    $data = array();
-    $sql = "SELECT * FROM vets";
-    $result = $conn->query($sql);
-  	if ($result->num_rows > 0) {
-        // output data of each row
-      	while($row = $result->fetch_array()){//loop to get all results
-            $data[] = $row;//grab everything and store inside array
-     	}
-    } else {
-        echo "0 results";
+ 
+// Attempt insert query execution
+    //$sql = "INSERT INTO persons (first_name, last_name, email) VALUES (" . $fName . ", " . $mName . ", " . $lName . ")";
+   echo $sql . "\n";
+// $sql = "INSERT INTO persons (first_name, last_name, email) VALUES ('Peter', 'Parker', 'no')";
+    //$sql = "INSERT INTO persons (first_name, last_name, email) VALUES ('gs', 'gr', 'gm')";
+    $sql = "INSERT INTO " . $table . " (idN, first, middle, last, branch, rank, activeD, phoneNum, email, dolcu, username, password) VALUES (" . $idN . ", " . $fName . ", " . $mName . ", " . $lName . ", " . $branc$
+    if($mysqli->query($sql) === true){
+        echo "Records inserted successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
     }
-    //access like a 2d array
-    return $data;
+ 
+    // Close connection
+    $mysqli->close();
 }
-function getVetGroup($idN, $group)
+  //* for all, commas for multiple
+  //order is optional: blank for nothing, ORDER BY row
+  //arr is for all of the cols
+function selectCol($cols, $table, $order, $arr)
 {
-    $sql = "SELECT " . $group . "FROM vets WHERE idN == " . $idN;
-    $result = $conn->query($sql);
-    return $result;
-}
-function setVetGroup($idN, $group, $value)
-{
-    $sql = "UPDATE vets SET " . $group . "=\'" . $value . "\' WHERE idN == " . $idN;
-    $result = $conn->query($sql);
-    return $result;
-}
-//Vendor functions (vendors)
-function addToVendor($idN, $fName, $mName, $lName, $type, $phoneNum, $email, $active, $password){
-    $sql = "INSERT INTO vendors (idN, fName, mName, lName, type, phoneNum, email, active, password)
-    VALUES ($idN, $fName, $mName, $lName, $type, $phoneNum, $email, $active, $password)";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        return true;
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-        return false;
-    }
-}
-function getAllVendorData()
-{
-  	$data = array();
-    $sql = "SELECT * FROM vendors";
-    $result = $conn->query($sql);
-  	if ($result->num_rows > 0) {
-        // output data of each row
-      	while($row = $result->fetch_array()){//loop to get all results
-            $data[] = $row;//grab everything and store inside array
-     	}
-    } else {
-        echo "0 results";
-    }
-    //access like a 2d array
-    return $data;
-}
-function getVendorGroup($idN, $group)
-{
-    $sql = "SELECT " . $group . "FROM vendors WHERE idN == " . $idN;
-    $result = $conn->query($sql);
+  $conn = new mysqli("localhost", "root", "briggs-test", "V4V");
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  } 
   
-    return $result;
-}
-function setVendorGroup($idN, $group, $value)
-{
-    $sql = "UPDATE vendors SET " . $group . "=\'" . $value . "\' WHERE idN == " . $idN;
-    $result = $conn->query($sql);
-    return $result;
-}
-//donor functions
-function addToDonor($idN, $amount, $purpose){
-    $sql = "INSERT INTO donor (idN, amount, purpose)
-    VALUES ($idN, $amount, $purpose)";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        return true;
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-        return false;
-    }
-}
-function getAllDonorData()
-{
-  	$data = array();
-    $sql = "SELECT * FROM donor";
-    $result = $conn->query($sql);
-  	if ($result->num_rows > 0) {
-        // output data of each row
-      	while($row = $result->fetch_array()){//loop to get all results
-            $data[] = $row;//grab everything and store inside array
-     	}
-    } else {
-        echo "0 results";
-    }
-    //access like a 2d array
-    return $data;
-}
-function getDonorGroup($idN, $group)
-{
-    $sql = "SELECT " . $group . "FROM donor WHERE idN == " . $idN;
-    $result = $conn->query($sql);
+  $sql = "SELECT " . $cols . " FROM " . $table . " " . $order;
+  $result = $conn->query($sql);
   
-    return $result;
-}
-function setDonorGroup($idN, $group, $value)
-{
-    $sql = "UPDATE donor SET " . $group . "=\'" . $value . "\' WHERE idN == " . $idN;
-    $result = $conn->query($sql);
-    return $result;
-}
-//experience functions
-function addToExperience($idN, $sName, $address, $website, $description, $datesA, $fees){
-    $sql = "INSERT INTO experience (idN, sName, address, website, description, datesA, fees)
-    VALUES ($idN, $sName, $address, $website, $description, $datesA, $fees)";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        return true;
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-        return false;
-    }
-}
-function getAllExperienceData()
-{
-  	$data = array();
-    $sql = "SELECT * FROM experience";
-    $result = $conn->query($sql);
-  	if ($result->num_rows > 0) {
-        // output data of each row
-      	while($row = $result->fetch_array()){//loop to get all results
-            $data[] = $row;//grab everything and store inside array
-     	}
-    } else {
-        echo "0 results";
-    }
-    //access like a 2d array
-    return $data;
-}
-function getExperienceGroup($idN, $group)
-{
-    $sql = "SELECT " . $group . "FROM experience WHERE idN == " . $idN;
-    $result = $conn->query($sql);
-  
-    return $result;
-}
-function setExperienceGroup($idN, $group, $value)
-{
-    $sql = "UPDATE experience SET " . $group . "=\'" . $value . "\' WHERE idN == " . $idN;
-    $result = $conn->query($sql);
-    return $result;
-}
-//vachome functions
-function addToVacHome($idN, $address, $REL, $description, $datesA, $bedrooms, $fees){
-    $sql = "INSERT INTO vacHome (idN, address, REL, description, datesA, bedrooms, fees)
-    VALUES ($idN, $address, $REL, $description, $datesA, $bedrooms, $fees)";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        return true;
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-        return false;
-    }
-}
-function getAllVacHomeData()
-{
-  	$data = array();
-    $sql = "SELECT * FROM vacHome";
-    $result = $conn->query($sql);
-  	if ($result->num_rows > 0) {
-        // output data of each row
-      	while($row = $result->fetch_array()){//loop to get all results
-            $data[] = $row;//grab everything and store inside array
-     	}
-    } else {
-        echo "0 results";
-    }
-    //access like a 2d array
-    return $data;
-}
-function getVacHomeGroup($idN, $group)
-{
-    $sql = "SELECT " . $group . "FROM vacHome WHERE idN == " . $idN;
-    $result = $conn->query($sql);
-  
-    return $result;
-}
-function setVacHomeGroup($idN, $group, $value)
-{
-    $sql = "UPDATE vacHome SET " . $group . "=\'" . $value . "\' WHERE idN == " . $idN;
-    $result = $conn->query($sql);
-    return $result;
-}
-function closeConnection()
-{
-    $conn->close();
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo "id: " . $row[$arr[0]]. " - Name: " . $row[$arr[1]]. " " . $row[$arr[2]]. " " . $row[$arr[2]]."<br>";
+      }
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
 }
 ?>
-<html>
-<body>
-hi
-<form method="post" enctype="multipart/form-data">
-	<input type="submit" value="Connect" name="connect">
-  	<input type="submit" value="ADD" name="ADD VET">
-    <input type="submit" value="disconnect" name="Disconnect">
-                  <span id="myspan"> OUTPUT </span>
-</form>
-</body>
-</html>
