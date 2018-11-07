79950cf6f7feca137aead2eed4decd99ea81b15e
Jack Briggs <briggsj21@stlukesct.org>
2018-11-07 15:39:31 -0500

    *

--- signupVet.php
+++ signupVet.php
@@ -90,7 +90,8 @@ if (isset($_POST['createVet']))
 				{
 					$newIdTemp = rand(100000, 1000000);
 					$newId = $newIdTemp;
-				}while(duplicateCol($newIdTemp, 'idN', 'vets'));
+				}while(false);
+				//duplicateCol($newIdTemp, 'idN', 'vets')
     			$array = array("idN" => $newId, "first" => '\'' . $_POST['firstname'] . '\'', "middle" => '\'' . $_POST['middlename'] . '\'', "last" => '\'' . $_POST['lastname'] . '\'', "branch" => '\'' . $_POST['branch'] . '\'', 
 				"rank" => '\'' . $_POST['rank'] . '\'', "activeD" => '\'' . $_POST['aD'] . '\'', "phoneNum" => '\'' . $_POST['phone'] . '\'', "email" => '\'' . $_POST['email'] . '\'', "dolcu" => '\'NEVER\'', 
 				"username" => '\'' . $_POST['username'] . '\'', "password" => '\'' . $_POST['password'] . '\'');
