51e5866c0ad75c070881822039d8e65f93e80e66
Jack Briggs <briggsj21@stlukesct.org>
2018-11-26 18:41:35 -0500

    *

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -5,7 +5,7 @@
 define("HOST", "localhost");     // The host you want to connect to.
 define("USER", "root");    // The database username. /*web_user*/
 define("PASSWORD", "briggs-test");    // The database password. YPEQ9ZGxAsa6GSTZr55M2K47c
-define("DATABASE", "`secure_login`");    // The database name.
+define("DATABASE", "secure_login");    // The database name.
  
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
