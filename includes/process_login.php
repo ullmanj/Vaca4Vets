cdbe1d056a2324b5a22afe14b8eacba9ab480f69
Jack Briggs <briggsj21@stlukesct.org>
2018-11-26 11:34:29 -0500

    *

--- includes/process_login.php
+++ includes/process_login.php
@@ -7,7 +7,7 @@ sec_session_start(); // Our custom secure way of starting a PHP session.
 if (isset($_POST['email'], $_POST['p'])) {
     $email = $_POST['email'];
     //$password = $_POST['p']; // The hashed password.
-    $password = $_POST['password'];
+    $password = $_POST['p'];
  	$result = login($email, $password, $mysqli);
     if ($result == 'success') {
         // Login success 
